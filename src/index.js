0dcabbb9c8554b218bb3efcb65ddbeb58429785b
Adam <worldelephantseal@gmail.com>
2021-04-16 11:45:01 +0100

    Revert "Disabled the 'Try It Out' feature of Swagger UI. It doesn't appear to be working for me currently, but that could be because I do not understand how it works"
    This reverts commit 2bae3a42c5b9d3e977310cfce9cfdf212ec1e9e4.

--- src/index.js
+++ src/index.js
@@ -3,25 +3,10 @@ import 'swagger-ui/dist/swagger-ui.css';
 
 const spec = require('./postbyte-api-spec-swagger.yaml');
 
-const DisableTryItOutPlugin = function() {
-  return {
-    statePlugins: {
-      spec: {
-        wrapSelectors: {
-          allowTryItOutFor: () => () => false
-        }
-      }
-    }
-  }
-}
-
 const ui = SwaggerUI({
   spec,
   dom_id: '#swagger',
   deepLinking: true,
-  plugins: [
-    DisableTryItOutPlugin
-  ]
 });
 
 ui.initOAuth({
